3a089b3bd5462aea765eda0a77622ba8569ae509
matheucampell <matheu.campbell.us@outlook.com>
2024-01-07 17:04:23 -0500

    two-minute classification with new combs

--- SignalProcessing/session_processing.py
+++ SignalProcessing/session_processing.py
@@ -6,25 +6,25 @@ import os
 from scipy.signal import ellip, sosfiltfilt
 
 cwd = os.getcwd()
-sesname = "session_01-07-24_520725"
-dpath = os.path.join(os.getcwd(), "Data", sesname, "data.csv")
+sesname = "session_01-07-24_934275"
+dpath = os.path.join(os.getcwd(), "Data", "NewCombs", sesname, "data.csv")
 dtable = np.loadtxt(dpath)
 
 Fs = 250
 T = 1 / Fs
 
 channels = [1, 6, 7]
-tstart = 35  # seconds
-tcutoff = 75  # seconds
+tstart = 1  # seconds
+tcutoff = 60  # seconds
 
-sstart = int(tstart * Fs)
-scutoff = int(tcutoff * Fs)
+sstart = int(tstart*Fs)
+scutoff = int(tcutoff*Fs)
 
 # Average all good channels (channels 2 and 6 (index 3 and 7) bad)
 allchan = dtable[channels, :]
 avgdata = np.mean(allchan, axis=0)
 normavg = avgdata - np.mean(avgdata)
-truncnorm = normavg[sstart + 1: scutoff]
+truncnorm = normavg[sstart+1: scutoff]
 
 L = len(truncnorm)
 tvec = np.arange(0, L) * T
@@ -43,7 +43,7 @@ filtavg = sosfiltfilt(sos, normavg[sstart + 1:])
 # Plot normalized channel average
 plt.figure()
 plt.subplot(3, 1, 1)
-plt.plot(tstart + tvec, filtavgtrunc)
+plt.plot(tstart + tvec, filtavgtrunc, linewidth='0.5')
 plt.grid()
 plt.title("Filtered Channel Average")
 plt.xlabel("Time (s)")
@@ -52,7 +52,7 @@ plt.xlabel("Time (s)")
 norm_fft = np.fft.fft(filtavgtrunc)
 norm_shift = np.fft.fftshift(norm_fft)
 plt.subplot(3, 1, 2)
-plt.plot(Fs / L * np.arange(-L / 2, L / 2), np.abs(norm_shift))
+plt.plot(Fs / L * np.arange(-L / 2, L / 2), np.abs(norm_shift), linewidth='0.5')
 plt.grid()
 plt.title("|FFT(processed)|")
 plt.xlabel("Frequency (Hz)")
@@ -64,13 +64,15 @@ psdx = (1 / (Fs * L)) * np.abs(norm_fft) ** 2
 psdx[1:-1] = 2 * psdx[1:-1]
 freq = np.arange(0, Fs / 2, Fs / L)
 plt.subplot(3, 1, 3)
-plt.plot(freq, 10 * np.log10(psdx))
+plt.plot(freq, 10 * np.log10(psdx), linewidth='0.5')
 plt.grid()
 plt.title("Periodogram Using FFT")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Power/Frequency (dB/Hz)")
 
-peaks, mags = mne.preprocessing.peak_finder(psdx)
+peak_thresh = (1/4)*(max(psdx)-min(psdx))
+
+peaks, mags = mne.preprocessing.peak_finder(psdx, thresh=peak_thresh)
 mags = np.round(mags, 2)
 print(f"Peaks: {sorted(list(zip(freq[peaks], mags)), key=lambda x: -x[1])}")
 
